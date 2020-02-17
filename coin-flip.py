b666b37311d1e340b5bb7dfff00487852014a92d
Luca Bazzea <luca.bazzea@gmail.com>
2020-02-17 12:46:46 +0200

    Finished value_assign()

--- coin-flip.py
+++ coin-flip.py
@@ -1,27 +1,35 @@
 import random
 from IPython.display import clear_output
 
-option1 = " "
-option2 = " "
-assign = " "
 
-def values():
+def value_assign():
 
-    while assign == " ":
-        assign = input("Would you like to assign values to the coin? y:Yes n:No").lower()
+        assign = " "
 
+        while assign != "y" and assign != "n":
+                assign = input("Would you like to assign values to the coin? [y,n]: ").lower()
+    
+        option1 = " "
+        option2 = " "
+    
         if assign == "y":
-            
+            while option1 == " ":
+                option1 = input("Heads: ")
+            while option2 == " ":
+                option2 = input("Tails: ")
+        else:
+                print("No")
 
+value_assign()
 
 def coin_flip():
 
     clear_output()
-        flip = random.randint(0,1)
+    flip = random.randint(0,1)
 
     if flip == 0:
             print(f"Heads {option1}")
-        else:
+    else:
             print(f"Tails {option2}")
 
 coin_flip()
